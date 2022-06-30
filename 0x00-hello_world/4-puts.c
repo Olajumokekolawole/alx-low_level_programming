22c818e2b91584920dc1b715be426d4d48779bd3
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-06-30 22:20:43 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point 
  *
- * Return : Success 
+ * Return : 0 (Success)
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle,\n");
 	return(0);
 }
