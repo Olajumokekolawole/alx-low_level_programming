a115a205e7e3ca86164a1b05eb98c04445ae102f
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-06-30 16:22:17 +0100

    committee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *
  * return: Always 0 (Success)
  */
-int main(void)
+int main(void)// there are no arguments taken by main
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
