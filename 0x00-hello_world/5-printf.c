9af2eaa292e8846bba6feffb4d4a4b6ee15ae564
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-06-30 16:25:06 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *
  * return: Always 0 (Success)
  */
-int main(void)// there are no arguments taken by main
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
