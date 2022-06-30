f85bb06cb5b9f66f058e2d7acfd451493760078f
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-01 00:19:15 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art"
  *
- * return: Always 0 (Success)
+ * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
