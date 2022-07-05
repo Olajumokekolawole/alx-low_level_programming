22e45752ec3546d281742f3e2c885ded5038d4ee
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-05 14:15:41 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*main - prints alphabet ten times
+*
+* Return: 0
+* /
+
+{
+	int l;
+	int n = 0;
+
+	while (n < 10)
+	{
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l)
+			l += 1;
+		}
+		_putchar(10);
+		n += 1;
+	}
+	return (0);
+}
