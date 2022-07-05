9e66367e4360793bd7f1d0d8e40133d58795cde1
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-05 07:36:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - prints alphabet in lowercase
+*
+* Return: 0
+*/
+
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l += 1;
+	}
+	_putchar ('\n');
+	return (0);
+}
