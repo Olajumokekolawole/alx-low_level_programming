c569767701a80d405767ad75dff5b56a4d1e8903
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-05 17:40:29 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - function that computes the absolute value of an integer
+*
+* @c:  is the int that will use for the argument of the function
+*
+* Return: 0
+*/
+int print_last_digit(int n)
+
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
+
