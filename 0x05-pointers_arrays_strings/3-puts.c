29eac2ce182e9d5245562c50d6f52d58ea52b4e7
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-12 15:53:53 -0700

    string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
-* _puts -> this is a function that puts
-* @str: a param to _puts function
+* _puts -> this is a function that prints a function 
+* @str: pointer to a string to print
+*
+* Return: 0
 */
+
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
