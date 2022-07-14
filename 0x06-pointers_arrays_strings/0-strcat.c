c60f32cc43417affd0298dcf77c0f48beb2618c0
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-14 04:18:20 -0700

    concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
-* char *_strcat - function to concatenate two strings
-*
-* Return: Always 0.
+* _strcat - function to concatenate two strings
+* @dest: char pointer
+* @src: char pointer
+* Return: char
 */
 
 char *_strcat(char *dest, char *src)
 {
-	char s1[58] = "Olajumoke ";
-	char s2[] = "Kolawole\n";
-	char *ptr;
+	int a = 0;
+	int b = 0;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0')
+	{
+		dest[a + b] = src[b];
+		b++;
+	}
+	dest[a + b] = '\0';
+	return (dest);
 }
