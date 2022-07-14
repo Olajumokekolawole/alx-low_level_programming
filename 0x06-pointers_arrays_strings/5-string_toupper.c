51a11c59f8aea3f29221369d7a5bfed2fb782aca
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-14 05:47:17 -0700

    change

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* string_toupper -  function to convert lower to upper
+* @s: char pointer
+* Return: int
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] >= 97 && s[j] <= 122)
+		{
+			s[j] =  s[j] - 32;
+		}
+	}
+	return (s);
+}
