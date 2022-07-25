a9062640ac483bcd5ccfcd82c5da368cf00f4029
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-07-25 05:23:48 -0700

    createarray

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* create_array - function that creates an array
+* @size: variable int with value of size
+* @c: character value to add in array
+* Return: pointer
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = (char *)malloc(size * sizeof(char));
+
+	if (size == 0 || ptr == NULL)
+	{
+		ptr = 0;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+	}
+	return (ptr);
+}
