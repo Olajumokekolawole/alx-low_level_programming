47a256259e390440fd0fc6dc1753c2a302c6a61c
olajumokekolawole <olajumokekolawole2022@gmail.com>
2022-08-15 21:45:40 -0700

    final debug

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,7 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - the number of unique nodes in 
+ * looped_listint_len - the number of unique nodes in
  * looped listint_t linked list.
  *
  * @head: ptr to head of listint_t to check.
